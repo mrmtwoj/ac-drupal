e069e5595343434fe0a4d793a963a1e69c7d66e6
Mr Mtwoj <mr.mtwoj@gmail.com>
2016-11-05 14:56:12 +0330

    Revert "drupal"
    This reverts commit 9126c1a0fd17d7047ed572ca3aed5f8ce1fd5c9a.

--- core/setting.pl
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/perl
-
-print "\n[+] Setting This Error Please Update Script Version\n";
-
--- modules/dirlisting.pl
+++
@@ -1,7 +0,0 @@
-#start license
-$response=$ua->get("$target/");
-$source=$response->decoded_content;
-if ($source =~ /<title>Index of/g) {
-	tprint("attachment directory has directory listing : \npath: $target/");
-}
-#end license
